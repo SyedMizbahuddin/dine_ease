6e5f7ea113a8f22efcb7cbd2d157ee5279f9781e
Syed Mizbahuddin <syed@nworx.ai>
2024-03-05 14:46:25 +0530

    Initial commit

---
+++ src/main/java/com/mizbah/ReastaurantManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mizbah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReastaurantManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReastaurantManagementApplication.class, args);
+	}
+
+}
